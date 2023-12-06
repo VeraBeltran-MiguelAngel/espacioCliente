0f74bb118f10507a29121f27771c1db239a1b2e1
Roghelio <145814012+Roghelio@users.noreply.github.com>
2023-12-05 18:15:47 -0600

    ok, login

--- index.php
+++ index.php
@@ -27,10 +27,22 @@ if (isset($_GET["credenciales"])) {
         $conexionBD,
         $consulta
     );
-    if (mysqli_num_rows($sqlData) > 0) {
-        $userData = mysqli_fetch_all($sqlData, MYSQLI_ASSOC);
-        echo json_encode($userData);
-        exit;
+    if (mysqli_num_rows($sqlData) > 0) { //Usuario existe en la tabla Clientes
+        //Validar que usuario exista se encuentre en la tabla detalle cliente
+        mysqli_next_result($conexionBD);//sirve para ejecutar el siguiente query en caso de haber 2 o más
+        $query = "SELECT dm.idDetMem FROM DetalleMembresia AS dm, Cliente AS c WHERE dm.Cliente_ID_Cliente=c.ID_Cliente AND c.pass='".$pass."' AND c.email='".$username."'";
+        $existDetalleMmebresia = mysqli_query($conexionBD, $query);
+
+        if(mysqli_num_rows($existDetalleMmebresia) > 0){ //Validar si existe 
+            $userData = mysqli_fetch_all($sqlData, MYSQLI_ASSOC);
+            echo json_encode($userData);
+            exit;
+        } else { //En caso de no existir el usuario dentro de la tabla detalle membresia
+            http_response_code(401);
+            echo json_encode(["message" => "Cuenta inactiva"]);
+            exit;
+        }
+        
     } else {
         http_response_code(401);
         echo json_encode(["message" => "Login invalido"]);
